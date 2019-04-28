1d4414ab067000a03634988e5832a86e7e1c048e
LeoooY <Leoyy3@163.com>
2019-04-28 09:45:42 +0800

    tell userAgent

---
+++ platFormByUserAgent.js
@@ -0,0 +1,25 @@
+const os = function () {
+  var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isChrome = /(?:Chrome|CriOS)/.test(ua),
+    isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)) ,
+    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+    isPc = !isPhone && !isAndroid && !isSymbian;
+  return {
+    isTablet: isTablet,
+    isPhone: isPhone,
+    isAndroid: isAndroid,
+    isPc: isPc
+  };
+}();
+
+if (os.isAndroid || os.isPhone) {
+  console.log("手机")
+} else if (os.isTablet) {
+  console.log("平板")
+} else if (os.isPc) {
+  console.log("电脑")
+}
