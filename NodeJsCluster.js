393861a3976f9544865541b3a17ef143ee35d146
LeoooY <Leoyy3@163.com>
2019-05-22 20:14:21 +0800

    cluster

--- NodeJsCluster.js
+++ NodeJsCluster.js
@@ -2,16 +2,23 @@ const cluster=require('cluster');
 const os=require('os');
 
 if(cluster.isMaster){
-  const numWorkers=require('os').cpus.length;
-  console.log('Master is setting up '+numWorkers+' workers...';
+  const numWorkers=require('os').cpus().length;
+  console.log('Master is setting up '+numWorkers+' workers...');
   debugger;
 
   for(let i=0,n=os.cpus().length; i<n ; i++){
     cluster.fork();    
+    debugger;
   }
 
   cluster.on('online',()=>{
-    console.log('Worker'+Worker)
+    console.log('Worker'+worker.process.pid+'is online');
+  })
+
+  cluster.on('exit',()=>{
+    console.log('Worker '+worker.process.pid+' died with code: ' + code + ', and signal: ' + signal)
+    console.log('Starting a new worker');
+    cluster.fork();
   })
 
 }else{
