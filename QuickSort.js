6099808caf55cae4b0a190318e9ed9f71616b63e
LeoooY <Leoyy3@163.com>
2019-06-17 08:22:40 +0800

    review quick sort

--- QuickSort.js
+++ QuickSort.js
@@ -1,3 +1,10 @@
+/* 
+  最好时间复杂度 O(NLogN)
+  平均时间复杂度 O(NlogN)
+  最坏时间复杂度 O(N^2)
+*/
+
+
 function selectPivot(arr){
   /* 选择枢纽的函数 */
    const idx=Math.floor(arr.length/2);
@@ -29,6 +36,31 @@ function quickSort(arr){
   
 
   
+}
+
+function quickSortPureES6(arr) {
+  if (!arr) {
+    return;
+  }
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const len = arr.length;
+  const pivotIdx = Math.floor(len / 2);
+  const pivot = arr.splice(pivotIdx, 1)[0];
+
+  const left = [],
+    right = [];
+
+  for (let el of arr) {
+    if (el > pivot) {
+      right.push(el);
+    } else {
+      left.push(el);
+    }
+  }
+
+  return quickSort(left).concat([pivot], quickSort(right));
 }
 
 console.log(quickSort([1,2,4,3,12,7,8,9]))
