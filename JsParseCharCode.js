2660ca0b5d624cf96ee8d96180cb8b7f6c2fd4cd
LeoooY <Leoyy3@163.com>
2019-12-18 09:04:11 +0800

    test over

--- JsParseCharCode.js
+++ JsParseCharCode.js
@@ -8,15 +8,23 @@ const parse = (str) => {
 
 | platform | software | ascii| character |
 | - | - | -| - |
-|linux |wps |10 |\n |
-|linux |石墨 |10 |\n |
-
-
+|linux | wps |10 |\n |
+|linux | 石墨 |10 |\n |
+|windows| excel |10|\n|
+|windows| wps |10|\n|
+|windows| 石墨 |10|\n|
+|Mac|numbers|10|\n|
 */
 
+const str=`123 
+2wer 
+123 
+1234 
+1234 
+3 
+1234`
+parse(str)
+console.log(str.replace(/\n/g,','))
 
-parse(`1
-2
-3
-4`)
 debugger;
+
